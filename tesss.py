ba7e065542dc2e2637b2072be938a002721ca2a6
arixara <65598399+arixara@users.noreply.github.com>
2020-06-12 11:47:10 +0300

    Генератор данных задач

--- tesss.py
+++ tesss.py
@@ -8,18 +8,21 @@ def generateO(myline):
     myline = str(na) + ',' + str(nb) + ',' + str(nc) + ',' + str(nd)
     return myline
 
-def generateD(a, k, myline):
-    i = random.randint(1, k - 1)
+def generateD(k):
+    myline = ''
+    a = [['o'] * k] * k
+    i = random.randint(0, k - 1)
     a[i][i] = 0
     i = random.randint(1, k - 1)
     j = random.randint(0, k - 1)
     a[i][j] = 0
-    for i in range(1, k + 1):
-        for j in range(0, k):
-            if a[i][j] == 'o':
+    for i in range(k):
+        for j in range(k):
+            if a[i][j] != 0:
                 a[i][j] = random.randint(-10, 10)
-        myline += str(*a[i]) + ' /n'
-    myline += ' /n'
+            myline += str(a[i][j]) + ' '
+        myline += ' \n'
+    myline += ' \n'
     return myline
 
 print("Сгенерировать файлы для задачи: ")
@@ -31,32 +34,33 @@ m = int(input())
 for i in range(m):
     a = str(i) + '.txt'
     file = open(a, "w", encoding='utf-8')
-    print("Величина файла (количество задач)", i, " = ")
+    print("Величина файла", i, "(количество задач) = ")
     n = int(input())
     file.write(str(n) + '\n')
     if p == 1:
-        print("Максимальная величина матрицы для файла ", i)
+        print("Величина матрицы для файла", i)
         k = int(input())
     for j in range (n):
         myline = ''
         if p == 2:
             myline = generateO(myline)
         else:
-            a = [['o','o','o'],['o'] * k] * k
-            nb = random.randint(1, k)
-            nc = random.randint(1, k)
+            nb = random.randint(1, (k ** 2))
+            nc = random.randint(1, (k ** 2))
             while (nb == nc):
-                nc = random.randint(1, k)
-            a[0] = str(k) + ' ' + str(nb) + ' ' + str(nc)
-            myline += str(a[0]) + ' /n'
-            myline = generateD(a, k, myline)
+                nc = random.randint(1, (k ** 2))
+            b = str(k) + ' ' + str(nb) + ' ' + str(nc)
+            mylinea = ''
+            mylinea = b + '\n'
+            mylineb = generateD(k)
+            myline = mylinea + mylineb
         print(myline)
         file.write(myline + ' \n')
     print()
-    print("Запись файла ", i, " завершена.")
+    print("Запись файла", i, "завершена.")
+    file.close()
     print()
 print()
 print()
 print("Удачи!")
-file.close()
 
